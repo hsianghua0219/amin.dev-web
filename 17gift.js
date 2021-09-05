9967c054fbc80cf86687f0d434573191919aeb0c
hsianghua0219 <60957985+hsianghua0219@users.noreply.github.com>
2021-09-06 02:43:02 +0800

    Update 17gift.js

--- 17gift.js
+++ 17gift.js
@@ -1,14 +1,11 @@
-const checkGift = function(){
-  document.getElementsByClassName('ChatList__ChatListWrapper-jeEuyN')[0].style.backgroundColor = '#EEFFEE';
+document.getElementsByClassName('ChatList__ChatListWrapper-jeEuyN')[0].style.backgroundColor = '#EEFFEE';
 
-  while (true) {
-      var chat = document.getElementsByClassName('Chat__ChatWrapper-dhHBjd');
-      if (chat[0].getElementsByClassName('GiftItem__GiftIcon-fZjCQ').length > 0){
-          chat[0].style.backgroundColor = '#fffeee';
-          chat[0].style.borderRadius = '15px';
-          chat[0].style.padding = '10px';
-      }
-      chat[0].classList.remove('Chat__ChatWrapper-dhHBjd');
-  }
+while (true) {
+    var chat = document.getElementsByClassName('Chat__ChatWrapper-dhHBjd');
+    if (chat[0].getElementsByClassName('GiftItem__GiftIcon-fZjCQ').length > 0){
+        chat[0].style.backgroundColor = '#fffeee';
+        chat[0].style.borderRadius = '15px';
+        chat[0].style.padding = '10px';
+    }
+    chat[0].classList.remove('Chat__ChatWrapper-dhHBjd');
 }
-checkGift();
